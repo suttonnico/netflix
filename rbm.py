e6d2fd9661c9758cd09f06ac8d9a59dc24269b45
suttonnico <suttonnico2@gmail.com>
2017-06-08 22:27:33 -0300

    Add files via upload

--- rbm.py
+++ rbm.py
@@ -3,6 +3,29 @@ import numpy as np
 #from numbapro import vectorize
 
 
+
+
+def bin_2_score(bin):
+    r = 0
+    if bin[0] == 1:
+        r = 1
+    else:
+        if bin[1] == 1:
+            r = 2
+        else:
+            if bin[2] == 1:
+                r = 3
+            else:
+                if bin[3] == 1:
+                    r = 4
+                else:
+                    if bin[4] ==1:
+                        r = 5
+                    else:
+                        r = 0
+    return r
+
+
 #@vectorize(["float32(int32,int32,float32,int32,bool,float32)"])
 def get_one_hid_p(m, K, W, j, V, visbiases):
     sum1 = 0
@@ -61,8 +84,11 @@ class SRBM:
                 if data_movie[i] == j:
                     cant += 1
                     self.hidbiases[j][:] += data_bscore[i][:]
-            self.hidbiases[j][:] /= cant
-            self.hidbiases[j][:] = np.log10(self.hidbiases[j][:])
+            if cant > 0:
+                self.hidbiases[j][:] /= cant * 5
+            for i in range(0,5):
+                self.hidbiases[j][i] = np.log(self.hidbiases[j][i]/(1-self.hidbiases[j][i]))
+
 
     def train(self, data, epochs):
         for epoch in range(0,epochs):
@@ -70,33 +96,53 @@ class SRBM:
             pos_hid_p = self.get_hid_p(data)
             pos_hid_states = pos_hid_p > np.random.rand(self.F)
             pos_asso = np.zeros([self.m, self.K, self.F])
-            for i in range(0,self.m):
-                if np.any(data[i][:]) == 1:
-                    for j in range(0,self.F):
-                        for k in range(0, self.K):
-                            pos_asso[i][k][j] = data[i][k]*pos_hid_p[j]
+            #for i in range(0, self.m):
+            #    if np.any(data[i][:]) == 1:
+            #        for j in range(0,self.F):
+            #            for k in range(0, self.K):
+            #                pos_asso[i][k][j] = data[i][k]*pos_hid_p[j]
             neg_asso = np.zeros([self.m, self.K, self.F])
             neg_vis_p = self.get_vis_p(pos_hid_states)
             neg_hid_p = self.get_hid_p(neg_vis_p)
+            #neg_hid_p = pos_hid_p
             for i in range(0,self.m):
                 if np.any(data[i][:]) == 1:
                     for j in range(0,self.F):
                         for k in range(0, self.K):
+                            pos_asso[i][k][j] = data[i][k] * pos_hid_p[j]
                             neg_asso[i][k][j] = neg_vis_p[i][k]*neg_hid_p[j]
+            #self.hidbiases += self.learning_rate*(data - neg_vis_p)
+            #self.visbiases += self.learning_rate*(pos_hid_p - neg_hid_p)
             self.W += self.learning_rate*(pos_asso-neg_asso)
-            #print self.predict(data, 1)
-
+#cambios linea 108  115 116
 
     def predict(self, V, q):
         pos_hid_p = self.get_hid_p(V)
-        #print pos_hid_p
         pos_hid_states = pos_hid_p > np.random.rand(self.F)
-        #print pos_hid_states
         neg_vis_p = self.get_vis_p(pos_hid_states)
         movie = neg_vis_p[int(q)][:]
-        #print movie
         th = np.max(movie)
         score = movie >= th
-        return score
+        return np.dot(movie,[1,2,3,4,5])
+
+    def predict_user(self, V, M):
+        pos_hid_p = self.get_hid_p(V)
+        pos_hid_states = pos_hid_p > np.random.rand(self.F)
+        neg_vis_p = self.get_vis_p(pos_hid_states)
+        movie = neg_vis_p
+        score = movie
+        for i in range(1, int(M)):
+            th = np.max(movie[i][:])
+            score[i][:] = movie[i][:] >= th
+        RSE = 0
+        count = 0
+        for i in range(1, int(M)):
+            sd = bin_2_score(V[i][:])
+            if sd != 0:
+                count +=1
+                RSE += (sd - bin_2_score(score[i][:]))**2
+        RSEN = RSE / count
+        return RSE
+
     def _logistic(self, x):
        return 1.0 / (1 + np.exp(-x))
